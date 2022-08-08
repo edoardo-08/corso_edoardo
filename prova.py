d2363e3460abcfde21e48eed0a23c716e4628d1e
edoardo-08 <93883538+edoardo-08@users.noreply.github.com>
2022-08-08 11:59:23 +0200

    Add files via upload

---
+++ prova.py
@@ -0,0 +1,17 @@
+print("insert name")
+name = input()
+print(name)
+
+print("insert psw")
+psw = input()
+print(psw)
+
+print("inserisci lunghezza lista")
+lenght = input()
+
+lista=[]
+
+for i in range(int(lenght)):
+    lista.append(int(input()))
+
+print(lista)
